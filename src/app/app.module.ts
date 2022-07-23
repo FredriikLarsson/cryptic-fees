d8d525bd8bc6098fa4fa3643e40ee6e4603ac465
OptiUserX <62377832+OptiUserX@users.noreply.github.com>
2022-07-23 20:21:54 +0200

    Prepared project to use primeng instead of angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
+    TitleComponent,
   ],
   imports: [
     BrowserModule,
-    MatGridListModule,
-    MatCardModule,
-    MatChipsModule,
-    FlexLayoutModule
   ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
